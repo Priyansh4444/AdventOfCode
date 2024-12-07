eb8f9feddee1540bf90536cc25710eb19a6f6db4
Priyansh Shah <Priyanshpokemon@gmail.com>
2024-12-07 02:15:30 -0800

    commiting optimal solution @benjamin-cates

--- 2024/src/bin/07.rs
+++ 2024/src/bin/07.rs
@@ -79,3 +79,59 @@ fn test_part1() {
 fn test_part2() {
     assert_eq!(part2(include_str!("../data/test/07.txt")), 11387);
 }
+
+// Much faster solution for part 2:
+
+// fn part2(input: &str) -> usize {
+//     let mut count = 0;
+//     for line in input.lines() {
+//         let (ans, right) = line.split_once(": ").unwrap();
+//         let ans = ans.parse::<i64>().unwrap();
+//         let nums = right
+//             .split(" ")
+//             .map(|v| v.parse::<i64>().unwrap())
+//             .collect::<Vec<i64>>();
+//         if works(nums.as_slice(), ans, true) {
+//             count += ans;
+//         }
+//     }
+//     count as usize
+// }
+
+// fn works(nums: &[i64], target: i64, part2: bool) -> bool {
+//     if nums.len() == 0 {
+//         return target == 0;
+//     }
+//     if target <= 0 {
+//         return false;
+//     }
+//     let last = nums.last().unwrap();
+
+//     // Multiplication case
+//     if target % last == 0 {
+//         if works(&nums[0..nums.len() - 1], target / last, part2) {
+//             return true;
+//         }
+//     }
+
+//     // Concatenation case
+//     if part2 {
+//         let concat_digits = (10i64).pow(last.ilog(10) + 1);
+//         if (target - last) % concat_digits == 0 {
+//             if works(&nums[0..nums.len() - 1], target / concat_digits, part2) {
+//                 return true;
+//             }
+//         }
+//     }
+
+//     // Addition case
+//     return works(&nums[0..nums.len() - 1], target - last, part2);
+// }
+
+// Why is it faster?
+// Because you are cutting of branches that are not possible way faster
+// than the other solution. The other solution is trying to calculate all
+// possible ways to get to the target number, but this solution is cutting
+// off branches that are not possible way faster.
+
+// credit: @benjamin-cates
