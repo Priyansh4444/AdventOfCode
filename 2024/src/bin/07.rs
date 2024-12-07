2a55cb7612e9a1ad750c7f29b3a79d501a6f70a4
Priyansh Shah <Priyanshpokemon@gmail.com>
2024-12-06 21:16:54 -0800

    done, kinda easy

--- 2024/src/bin/07.rs
+++ 2024/src/bin/07.rs
@@ -1,15 +1,70 @@
 fn main() {
-    let input: &str = include_str!("../data/07.txt");
+    let input: &str = &include_str!("../data/07.txt").replace("\r", "");
     println!("Answer to part1: {}", part1(input));
     println!("Answer to part2: {}", part2(input));
 }
 
 fn part1(input: &str) -> usize {
-    0
+    let mut sum = 0;
+    for line in input.lines() {
+        let parts: Vec<&str> = line.split(": ").collect();
+        let rhs = parts[0].parse::<usize>().unwrap();
+        let lhs: Vec<usize> = parts[1]
+            .split_whitespace()
+            .map(|n| n.parse().unwrap())
+            .collect();
+        if can_make_value(rhs, &lhs, false) {
+            sum += rhs;
+        }
+    }
+    sum
+}
+
+fn can_make_value(target: usize, numbers: &[usize], part2: bool) -> bool {
+    if numbers.len() == 1 {
+        return numbers[0] == target;
+    }
+    let mut ops = vec!["+", "*"];
+    if part2 {
+        ops.push("||");
+    }
+    let mut expressions = vec![(numbers[0], vec![])];
+
+    for &num in &numbers[1..] {
+        let mut new_expressions = vec![];
+        for (val, ops_used) in expressions {
+            for &op in &ops {
+                let new_val = match op {
+                    "+" => val + num,
+                    "*" => val * num,
+                    "||" => (val.to_string() + &num.to_string()).parse().unwrap(),
+                    _ => unreachable!(),
+                };
+                let mut new_ops: Vec<&str> = ops_used.clone();
+                new_ops.push(op);
+                new_expressions.push((new_val, new_ops));
+            }
+        }
+        expressions = new_expressions;
+    }
+
+    expressions.iter().any(|(val, _)| *val == target)
 }
 
 fn part2(input: &str) -> usize {
-    0
+    let mut sum = 0;
+    for line in input.lines() {
+        let parts: Vec<&str> = line.split(": ").collect();
+        let rhs = parts[0].parse::<usize>().unwrap();
+        let lhs: Vec<usize> = parts[1]
+            .split_whitespace()
+            .map(|n| n.parse().unwrap())
+            .collect();
+        if can_make_value(rhs, &lhs, true) {
+            sum += rhs;
+        }
+    }
+    sum
 }
 
 #[test]
