a216d78bb1978145ced991902ec036112ea35fc0
Priyansh Shah <Priyanshpokemon@gmail.com>
2024-12-09 00:17:57 -0800

    using new soln

--- 2024/src/bin/09.rs
+++ 2024/src/bin/09.rs
@@ -63,78 +63,42 @@ fn part1(input: &str) -> usize {
 
 fn part2(input: &str) -> usize {
     let input = input.to_string() + "0";
-
-    let pairs = input
-        .chars()
-        .map(|c| c.to_digit(10).unwrap() as usize)
-        .collect::<Vec<usize>>()
-        // split into (block size, free size)
+    let mut pairs = vec![];
+    input
+        .as_bytes()
         .chunks(2)
-        .map(|c| (c[0], c[1]))
-        .collect::<Vec<(usize, usize)>>();
-        let mut disk = Vec::new();
-        let mut file_sizes = Vec::new();
-        let mut file_id = 0;
-        let mut free_sizes = Vec::new();
-        for (file_size, free_space) in pairs {
-            file_sizes.push(file_size);
-            free_sizes.push(free_space);
-            for _ in 0..file_size {
-                disk.push(Some(file_id));
-            }
-            for _ in 0..free_space {
-                disk.push(None);
-            }
-            file_id += 1;
-        }
-
+        .enumerate()
+        .for_each(|(chunk_id, a)| {
+            pairs.push(((a[0] - b'0') as usize, Some(chunk_id)));
+            pairs.push(((a[1] - b'0') as usize, None));
+        });
+    let mut i = pairs.len() - 1;
     // check in decreasing order of file_id
-    for current_id in (0..file_id).rev() {
-        // find the size of the file (going bacwards)
-        let file_size = file_sizes[current_id];
-        // find the index where the file starts
-        let file_start = disk.iter().position(|&x| x == Some(current_id)).unwrap();
-        let mut best_pos = None;
-
-        // Try to find if you can put a file of the biggest id, at the start of the disk or leave it at the end
-        let free_spaces = disk[..file_start]
-            .iter()
-            .enumerate()
-            .collect::<Vec<_>>() // collect to Vec to use slice methods
-            .chunk_by(|(_, a), (_, b)| a.is_none() == b.is_none()) // group by None/Some
-            .into_iter()
-            .filter(|chunk| chunk[0].1.is_none()) // keep only None chunks
-            .map(|chunk| {
-                let start_pos = chunk[0].0;
-                let length = chunk.len();
-                (start_pos, length)
-            })
-            .collect::<Vec<_>>();
-
-        for (start_pos, length) in free_spaces {
-            if length >= file_size {
-                best_pos = Some(start_pos);
-                break;
+    while i != 0 {
+        if let (size, Some(_)) = pairs[i] {
+            if let Some(new_pos) = pairs
+                .iter()
+                .take(i)
+                .position(|v| v.1 == None && v.0 >= size)
+            {
+                let free_size = pairs[new_pos].0;
+                pairs[new_pos] = pairs[i];
+                pairs[i].1 = None;
+                pairs.insert(new_pos + 1, (free_size - size, None));
+                continue;
             }
         }
-        // If we found a space big enough move the fiile
-        if let Some(new_pos) = best_pos {
-            // Clear old position
-            for i in file_start..file_start + file_size {
-                disk[i] = None;
-            }
-            // Place at new position
-            for i in new_pos..new_pos + file_size {
-                disk[i] = Some(current_id);
-            }
-        }
+        i -= 1;
     }
 
     // Calculate checksum
-    disk.iter()
+    pairs
+        .iter()
+        .map(|v| std::iter::repeat_n(v.1.unwrap_or(0), v.0))
+        .flatten()
         .enumerate()
-        .filter_map(|(pos, &file_id)| file_id.map(|id| pos * id))
-        .sum()
+        .map(|(i, v)| i * (v as usize))
+        .sum::<usize>()
 }
 
 #[test]
